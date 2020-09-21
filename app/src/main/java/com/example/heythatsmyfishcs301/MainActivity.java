a3934ad949d4f538fc804334ed3d104393007e0b
Linda <nguyenli23@up.edu>
2020-09-21 16:18:18 -0700

    Added background and trying to draw fish for scores

--- app/src/main/java/com/example/heythatsmyfishcs301/MainActivity.java
+++ app/src/main/java/com/example/heythatsmyfishcs301/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.heythatsmyfishcs301;
 
 import android.app.Activity;
+import android.graphics.PixelFormat;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+
+import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,9 +16,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    //SurfaceView gameView = findViewById(R.id.boardView);
+
+
+        //SurfaceView sfvTrack = (SurfaceView)findViewById(R.id.boardView);
+        //sfvTrack.setZOrderOnTop(true);    // necessary
+        //SurfaceHolder sfhTrackHolder = sfvTrack.getHolder();
+        //sfhTrackHolder.setFormat(PixelFormat.TRANSPARENT);
+    }
 
 
 }
