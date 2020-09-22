2fda4c9ffe24e0faf1f6aa9608d637eed2e34a91
Linda <nguyenli23@up.edu>
2020-09-21 21:47:03 -0700

    Added random number of fish to the tiles.

--- app/src/main/java/com/example/heythatsmyfishcs301/hexagonPattern.java
+++ app/src/main/java/com/example/heythatsmyfishcs301/hexagonPattern.java
@@ -27,6 +27,7 @@ public class hexagonPattern extends SurfaceView {
     private Rect tile;
     Bitmap redPenguin = null;
     Bitmap resizedRedPenguin = null;
+
     Bitmap oneFish = null;
     Bitmap twoFish = null;
     Bitmap threeFish = null;
@@ -36,8 +37,10 @@ public class hexagonPattern extends SurfaceView {
     Bitmap rThreeFish = null;
 
     private int randTile;
+
     Bitmap orangePenguin = null;
     Bitmap resizedOrangePenguin = null;
+
 
     private Paint testPaint = new Paint();
 
@@ -54,8 +57,17 @@ public class hexagonPattern extends SurfaceView {
         ((SurfaceHolder) sfhTrackHolder).setFormat(PixelFormat.TRANSPARENT);
 
         testPaint.setColor(Color.WHITE);
+        oneFish = BitmapFactory.decodeResource(getResources(), R.drawable.onefishtile);
+        twoFish = BitmapFactory.decodeResource(getResources(), R.drawable.twofishtile);
+        threeFish = BitmapFactory.decodeResource(getResources(), R.drawable.threefishtile);
+        rOneFish = Bitmap.createScaledBitmap(oneFish, 50, 50, false);
+        rTwoFish = Bitmap.createScaledBitmap(twoFish, 50, 50, false);
+        rThreeFish = Bitmap.createScaledBitmap(threeFish, 50, 50, false);
+
         redPenguin = BitmapFactory.decodeResource(getResources(), R.drawable.redpenguin);
-        resizedRedPenguin = Bitmap.createScaledBitmap(redPenguin, 150, 150, false);
+        resizedRedPenguin = Bitmap.createScaledBitmap(redPenguin, 115, 115, false);
+        orangePenguin = BitmapFactory.decodeResource(getResources(), R.drawable.orangepenguin);
+        resizedOrangePenguin = Bitmap.createScaledBitmap(orangePenguin, 115, 115, false);
 
     }
 
@@ -84,6 +96,11 @@ public class hexagonPattern extends SurfaceView {
         //This loop will draw the hexagonal array.
         for(int i=0;i<=8;i++) {
 
+            Random rand = new Random();
+            randTile = rand.nextInt(3);
+
+
+
             //Draws even rows
             if (i%2==0) {
                 numRows = 8;
@@ -102,29 +119,72 @@ public class hexagonPattern extends SurfaceView {
                 hex.computeHex(tile);
                 hex.draw(c);
 
+                if(randTile == 1){
+                    c.drawBitmap(this.rOneFish, (float)tile.left + tile.right / 2, (float)(tile.top + tile.bottom) / 2, null);
+                }
+                else if(randTile == 2){
+                    c.drawBitmap(this.rTwoFish, (float)tile.left + tile.right / 2, (float)(tile.top + tile.bottom) / 2, null);
+                }
+                else if(randTile == 3){
+                    c.drawBitmap(this.rThreeFish, (float)tile.left + tile.right / 2, (float)(tile.top + tile.bottom) / 2, null);
+                }
 
 
                 //TEMPORARY CODE FOR HW 1
                 //This is an example of drawing a small white circle on a specific hexagon:
-                if (i == 1 && j == 0) {
-                    testPaint.setColor(Color.WHITE);
+               // if (i == 1 && j == 0) {
+                   // testPaint.setColor(Color.WHITE);
                     //you can access the location of the hexagons with tile.left, tile.right, etc.
-                    c.drawCircle((tile.left + tile.right) / 2, (tile.top + tile.bottom) / 2, hexWidth / 4, testPaint);
-                }
+                    //c.drawCircle((tile.left + tile.right) / 2, (tile.top + tile.bottom) / 2, hexWidth / 4, testPaint);
+               // }
 
                 //If you wanted to access the top left corner of the hexagon, you would use: (tile.left, tile.top)
 
+
                 //If you want to draw it to multiple hexagons you could do something like this:
-                if ((i+j)%3 == 0)
+                if ( i == 3 && j == 0)
                 {
+                    c.drawBitmap(this.resizedRedPenguin, 0.1f*hexWidth+tile.left, tile.top, null);
+                }
+                else if(i == 2 && j == 0)
+                {
+                    c.drawBitmap(this.resizedOrangePenguin, 0.1f*hexWidth+tile.left, tile.top, null);
+                }
+                else if(i == 6 && j == 4)
+                {
+                    c.drawBitmap(this.resizedRedPenguin, 0.1f*hexWidth+tile.left, tile.top, null);
+                }
+                else if(i == 3 && j == 4)
+                {
+                    c.drawBitmap(this.resizedOrangePenguin, 0.1f*hexWidth+tile.left, tile.top, null);
+                }
+                else if(i == 5 && j == 1)
+                {
+                    c.drawBitmap(this.resizedRedPenguin, 0.1f*hexWidth+tile.left, tile.top, null);
+                }
+                else if(i == 7 && j == 2)
+                {
+                    c.drawBitmap(this.resizedOrangePenguin, 0.1f*hexWidth+tile.left, tile.top, null);
+                }
+                else if(i == 4 && j == 3)
+                {
+                    c.drawBitmap(this.resizedRedPenguin, 0.1f*hexWidth+tile.left, tile.top, null);
+                }
+                else if(i == 1 && j == 6)
+                {
+
                     testPaint.setColor(Color.GREEN);
                     c.drawBitmap(this.resizedRedPenguin, 0.0f, 300.0f, null);
-                    c.drawCircle((tile.left + tile.right) / 2, (tile.top + tile.bottom) / 2, hexWidth / 4, testPaint);
+                    //c.drawCircle((tile.left + tile.right) / 2, (tile.top + tile.bottom) / 2, hexWidth / 4, testPaint);
+
+                    c.drawBitmap(this.resizedOrangePenguin, 0.1f*hexWidth+tile.left, tile.top, null);
+
                 }
 
                 //We will absolutely not do it this way, but for now we can index hexagons like this.
                 //I think in the future we will create a real Tile class which will contain instance variables like: numFish, isGone, and have methods like getNeighbors().
                 //I think this is sufficient for now, and just call me if there's anything you need, Ryan
+
 
 
 
