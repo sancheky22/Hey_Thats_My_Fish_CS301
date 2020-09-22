500d55fc8514643bc13428528336bac298c511c6
Linda <nguyenli23@up.edu>
2020-09-21 21:49:06 -0700

    Added random number of fish to the tiles.

--- app/src/main/java/com/example/heythatsmyfishcs301/hexagonPattern.java
+++ app/src/main/java/com/example/heythatsmyfishcs301/hexagonPattern.java
@@ -190,7 +190,6 @@ public class hexagonPattern extends SurfaceView {
 
                 tile.left += hexWidth;
                 tile.right += hexWidth;
-                //fdgg
             }
 
             //Reset hexagon values after row is drawn.
