063be47f5352d86733a3ec6d01ba955a3e58c082
Linda <nguyenli23@up.edu>
2020-11-02 17:52:53 -0800

    Fixed it so there is only 2 default players

---
+++ app/src/main/java/com/example/heythatsmyfishcs301/fish/FishComputerPlayer2.java
@@ -0,0 +1,32 @@
+package com.example.heythatsmyfishcs301.fish;
+
+import com.example.heythatsmyfishcs301.game.GameComputerPlayer;
+import com.example.heythatsmyfishcs301.game.infoMsg.GameInfo;
+
+import java.util.Random;
+
+public class FishComputerPlayer2 extends GameComputerPlayer {
+    //constructor done!
+    public FishComputerPlayer2(String name){
+        super(name);
+    }
+
+    //Computer Player 1 sends a random action to the game state.
+    @Override
+    protected void receiveInfo(GameInfo info) {
+        FishGameState copy = new FishGameState((FishGameState) info);
+        Random r = new Random();
+        if (copy.getPlayerTurn() != this.playerNum){
+            return;
+        }
+        //If the game phase is mid-game
+        else if (copy.getGamePhase() == 1){
+
+
+        }
+        //If the game phase is set up
+        else{
+
+        }
+    }
+}
