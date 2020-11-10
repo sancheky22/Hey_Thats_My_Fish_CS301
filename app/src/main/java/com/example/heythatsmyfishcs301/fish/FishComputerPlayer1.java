1020c5d0935cd1932665787f558ee1ed585b228d
Kyle Sanchez <sancheky22@up.edu>
2020-11-09 21:09:43 -0800

    Working on ComputerPlayer 1. Added loop to check if current board piece is null and log debugging line

--- app/src/main/java/com/example/heythatsmyfishcs301/fish/FishComputerPlayer1.java
+++ app/src/main/java/com/example/heythatsmyfishcs301/fish/FishComputerPlayer1.java
@@ -1,5 +1,7 @@
 package com.example.heythatsmyfishcs301.fish;
 
+import android.util.Log;
+
 import com.example.heythatsmyfishcs301.game.GameComputerPlayer;
 import com.example.heythatsmyfishcs301.game.infoMsg.GameInfo;
 
@@ -16,29 +18,40 @@ public class FishComputerPlayer1 extends GameComputerPlayer {
     @Override
     protected void receiveInfo(GameInfo info) {
         FishGameState copy = new FishGameState((FishGameState) info);
-        Random r = new Random();
-        if (copy.getPlayerTurn() != this.playerNum){
+        //Random r = new Random();
+        if (copy.getPlayerTurn() != this.playerNum)
             return;
-        }
+
         //If the game phase is mid-game (Moving penguins)
         else if (copy.getGamePhase() == 1){
             //total penguins
             //0  to length of array-1 = row
             //0 to height of array-1 = column
 
-            // FishPenguin random locations
+            // FishPenguin random locations/
+            /*
             int penguinRandRow = (int) (copy.getPieceArray().length-1 * Math.random());
             int penguinRandCol = (int) (copy.getPieceArray().length-1 * Math.random());
 
             // FishTile random locations
             int tileRandRow = (int) (copy.getBoardState().length-1 * Math.random());
             int tileRandCol = (int) (copy.getBoardState().length-1 * Math.random());
+            */
 
+            // using our copy of gamestate
+            FishTile[][] pieceBoard = copy.getBoardState();
+            // loop through
+            for(int i =0; i < pieceBoard.length; i++){
+                for(int j=0; j< pieceBoard[i].length;j++){
+                    if(pieceBoard[i][j]==null){
 
-            //TODO: Make a check so the computer player only randomly selects from it's Penguin pieces and a valid Tile
+                    }
+                }
 
+            }
 
-            //FishMoveAction moveAction = new FishMoveAction(this,copy,);
+            Log.d("Move","Computer Player Moving");
+            //FishMoveAction moveAction = new FishMoveAction(this,copy.getPieceArray()[1][0], );
             //game.sendAction(moveAction);
         }
         //If the game phase is set up (Placing Penguins)
