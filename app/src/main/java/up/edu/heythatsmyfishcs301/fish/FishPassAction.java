7e6a5602716cb8078ffa394344ec694d347d8133
Kyle Sanchez <sancheky22@up.edu>
2020-11-21 22:42:49 -0800

    Added PassAction to allow a turn skip after a players penguins get removed from the board.

---
+++ app/src/main/java/up/edu/heythatsmyfishcs301/fish/FishPassAction.java
@@ -0,0 +1,23 @@
+package up.edu.heythatsmyfishcs301.fish;
+
+import up.edu.heythatsmyfishcs301.game.GamePlayer;
+import up.edu.heythatsmyfishcs301.game.actionMsg.GameAction;
+
+/**
+ * @author Kyle Sanchez
+ * @author Ryan Enslow
+ * @author Carina Pineda
+ * @author Linda Nguyen
+ **/
+public class FishPassAction extends GameAction{
+    /**
+     * constructor for GameAction
+     *
+     * @param player the player who created the action
+     */
+    public FishPassAction(GamePlayer player) {
+        super(player);
+    }
+
+
+}
